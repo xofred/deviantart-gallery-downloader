a0746633a92c0dcc982259bd872204c8e739bea2
xofred <fwin3000@gmail.com>
2014-08-12 01:19:46 +0800

    Improve showing messages.

--- with_login.rb
+++ with_login.rb
@@ -24,21 +24,21 @@ Dir.mkdir("deviantart") unless File.exists?("deviantart") do
   end
 end
 
-puts "Connecting to deviantART..."
+puts "Connecting to deviantART"
 HEADERS_HASH = {"User-Agent" => "Ruby/#{RUBY_VERSION}"}
 agent = Mechanize.new
 agent.get(HOME_URL)
 agent.pluggable_parser.default = Mechanize::Download
 
 # Login 
-puts "Logging in..." 
+puts "Logging in" 
 agent.page.form_with(:id => 'form-login') do |f|
   f.username = ARGV[0]
   f.password = ARGV[1]
 end.click_button
 
 # Go to the gallery 
-puts "Connecting to gallery..."
+puts "Connecting to gallery"
 agent.get(GALLERY_URL)
 
 # Find page link
@@ -48,14 +48,14 @@ if agent.page.parser.css('li.number').last
   last_page_number = agent.page.parser.css('li.number').last.text.to_i
 
   # Page 1
-  puts "Analyzing #{GALLERY_URL}..." 
+  puts "(1/#{last_page_number})Analyzing #{GALLERY_URL}"
   page_links = (agent.page.parser.css("a.thumb") || agent.page.parser.css("a.thumb ismature")).map{|a| a["href"]}
 
   # Page 2 to last
   for pg_number in 2..last_page_number do 
     offset = (pg_number - 1) * 24
     gallery_link = GALLERY_URL + "?offset=" + offset.to_s 
-    puts "Analyzing #{gallery_link}..." 
+    puts "(#{pg_number}/#{last_page_number})Analyzing #{gallery_link}" 
     agent.get(gallery_link)
     page_link = (agent.page.parser.css("a.thumb") || agent.page.parser.css("a.thumb ismature")).map{|a| a["href"]}
     page_links << page_link 
@@ -63,7 +63,7 @@ if agent.page.parser.css('li.number').last
   page_links.flatten!
 else
   # Page 1
-  puts "Analyzing #{GALLERY_URL}..." 
+  puts "Analyzing #{GALLERY_URL}" 
   page_links = (agent.page.parser.css("a.thumb") || agent.page.parser.css("a.thumb ismature")).map{|a| a["href"]}
 end
 
@@ -71,10 +71,11 @@ end
 for index in 1..page_links.count
   agent.get(page_links[index - 1])
   download_link = agent.page.parser.css(".dev-page-button.dev-page-button-with-text.dev-page-download").map{|a| a["href"]}[0]
+  title = agent.page.parser.css(".dev-title-container h1 a").text
   
   # Download
   begin 
-    puts "(#{index}/#{page_links.count})Downloading #{download_link.split('?').first}..."
+    puts "(#{index}/#{page_links.count})Downloading \"#{title}\""
     file_name = download_link.split('?').first.split('/').last
     file_path = "deviantart/#{AUTHOR_NAME}/#{GALLERY_NAME}/#{file_name}"
     agent.get(download_link).save(file_path) unless File.exist?(file_path) 
